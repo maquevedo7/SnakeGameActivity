c564612813faa9ccc5b2bb7e7ddcc129917662d6
NathalyBotina <111409434+NathalyArtesYLetras@users.noreply.github.com>
2022-10-06 00:22:37 -0500

    First change to convert cell to a new class
    First change to convert cell to a new class

--- SnakeGameOne/Program.cs
+++ SnakeGameOne/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnakeGameOne.Entities;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -272,53 +273,6 @@ namespace SnakeGameOne
             }
             Console.WriteLine(toPrint);
         }
-        public class Cell
-        {
-            public string val
-            {
-                get;
-                set;
-            }
-            public int x
-            {
-                get;
-                set;
-            }
-            public int y
-            {
-                get;
-                set;
-            }
-            public bool visited
-            {
-                get;
-                set;
-            }
-            public int decay
-            {
-                get;
-                set;
-            }
-
-            public void decaySnake()
-            {
-                decay -= 1;
-                if (decay == 0)
-                {
-                    visited = false;
-                    val = " ";
-                }
-            }
-
-            public void Clear()
-            {
-                val = " ";
-            }
-
-            public void Set(string newVal)
-            {
-                val = newVal;
-            }
-        }
+        
     }
 }
